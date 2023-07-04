0150fd16eb45f7ac8ab897efc1f3af9008f43f4c
Pavel Jakovlev <pavel.jakovlev@nortal.com>
2023-07-04 20:02:14 +0300

    NOJIRA Adding Swagger authentication + problem details

--- backend-app/HotelBookingSystem/Startup.cs
+++ backend-app/HotelBookingSystem/Startup.cs
@@ -1,4 +1,12 @@
-﻿namespace HotelBookingSystem.API
+﻿using System.Net.Mime;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Hellang.Middleware.ProblemDetails;
+using Hellang.Middleware.ProblemDetails.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace HotelBookingSystem.API
 {
     public class Startup
     {
@@ -6,13 +14,73 @@
         {
             Configuration = configuration;
         }
+
         public IConfigurationRoot Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new ProducesAttribute(MediaTypeNames.Application.Json));
+                options.Filters.Add(new ConsumesAttribute(MediaTypeNames.Application.Json));
+            });
+
+            services.AddProblemDetails(opts =>
+            {
+                opts.MapToStatusCode<Exception>(StatusCodes.Status500InternalServerError);
+            });
+            services.AddProblemDetailsConventions();
+
+            services.AddControllers().AddJsonOptions(opts =>
+            {
+                opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Hotel Booking System POC", Version = "v1", Description = "Hotel Booking System POC"
+                    });
+                c.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Description = @"Authorization header using the Bearer scheme.
+                                    <br>Enter token in the text input below <i>(Do not enter word 'Bearer')</i>.",
+                        Name = "auth-access-token",
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "Bearer"
+                    });
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference =
+                                    new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" },
+                                Scheme = "oauth2",
+                                Name = "Bearer",
+                                In = ParameterLocation.Header
+                            },
+                            new List<string>()
+                        }
+                    });
+
+                string xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                c.UseAllOfToExtendReferenceSchemas();
+            });
         }
+
         public void Configure(IApplicationBuilder app)
         {
+            app.UsePathBase("/api");
             app.UseRouting();
             app.UseEndpoints(x => x.MapControllers());
         }
