18b65c49a7c6c6108bde4a159137e8375c692546
Kęstutis Grumodas <Kestutis.Grumodas@nortal.com>
2023-07-04 11:27:08 +0300

    Add Startup.cs file

---
+++ backend-app/HotelBookingSystem/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace HotelBookingSystem.API
+{
+    public class Startup
+    {
+        public Startup(IConfigurationRoot configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(x => x.MapControllers());
+        }
+    }
+}
