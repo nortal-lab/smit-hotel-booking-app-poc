90ebfcbc284f07b97c9238e0a2c76fdaab6207f7
Kęstutis Grumodas <kestutis.grumodas@nortal.com>
2023-07-05 09:18:20 +0300

    Feature/ltbench 816 set up swagger

---
+++ backend-app/HotelBookingSystem/Models/RoomDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelBookingSystem.API.Models
+{
+    public class RoomDto
+    {
+        public Guid RoomId { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public int Capacity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
