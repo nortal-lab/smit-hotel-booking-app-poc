6ae9f6652004067c512e26a20b6f9145cacc6003
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-12 02:04:55 +0300

    LTBENCH-841 Added a PageContentWrapperComponent UI component

--- frontend-app/src/app/app-ui/app-ui.module.ts
+++ frontend-app/src/app/app-ui/app-ui.module.ts
@@ -6,10 +6,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppStepsComponent } from './steps/steps/steps.component';
 import { AppStepComponent } from './steps/step/step.component';
 import { AppStepPanelComponent } from './steps/step-panel/step-panel.component';
+import { PageContentWrapperComponent } from './page-content-wrapper/page-content-wrapper.component';
+
+const components = [
+  AppStepsComponent, 
+  AppStepComponent, 
+  AppStepPanelComponent,
+  PageContentWrapperComponent
+];
 
 @NgModule({
-  declarations: [AppStepsComponent, AppStepComponent, AppStepPanelComponent],
-  exports: [AppStepsComponent, AppStepComponent, AppStepPanelComponent],
+  declarations: [...components],
+  exports: [...components],
   providers: [],
   imports: [UiModule, FormsModule, CommonModule, HttpClientModule],
 })
