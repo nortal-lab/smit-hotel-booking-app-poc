cece09af1380db74029f70fcc86c795ecde8dddd
Ekaterina Loginova <ekaterina.loginova@nortal.com>
2023-07-12 14:49:01 +0300

    LTBENCH-844|Added TitleContainerComponent with removed extra slot

--- frontend-app/src/app/app-ui/title-container/title-container.component.stories.ts
+++ frontend-app/src/app/app-ui/title-container/title-container.component.stories.ts
@@ -11,19 +11,14 @@ export default {
     title: {
       name: 'Title',
     },
-    withRightContent: {
+    withContentRight: {
       name: 'With content on a right',
-      type: 'boolean',
-    },
-    withLeftContent: {
-      name: 'With content on a left',
       type: 'boolean',
     },
   },
   args: {
     title: 'Title of the title container(hi!)',
-    withRightContent: false,
-    withLeftContent: false,
+    withContentRight: false,
   },
 } as Meta;
 
@@ -33,16 +28,11 @@ const Template: StoryFn<AppTitleContainerComponent> = (args: AppTitleContainerCo
     ...args,
   },
   template: `
-      <app-title-container title="Title of the title container(hi!)" [withRightContent]="withRightContent" [withLeftContent]="withLeftContent">
-        <ng-container [titleContainerLeft]>
-          <cvi-ng-button [size]="'s'">and a button</cvi-ng-button>
-        </ng-container>
-        <ng-container [titleContainerRight]>
+      <app-title-container title="Title of the title container(hi!)" [withContentRight]="withContentRight">
           <cvi-ng-track [gap]="2" verticalAlignment="center">
             <span><b>with some extra content on the right</b></span>
             <cvi-ng-button [size]="'m'">and a button</cvi-ng-button>
           </cvi-ng-track>
-       </ng-container>
       </app-title-container>
     `,
 });
@@ -55,14 +45,6 @@ export const WithContentRight = {
   ...Default,
   name: 'With content on a right',
   args: {
-    withRightContent: true,
-  },
-};
-
-export const WithContentLeft = {
-  ...Default,
-  name: 'With content on a left',
-  args: {
-    withLeftContent: true,
+    withContentRight: true,
   },
 };
--- frontend-app/src/app/app-ui/title-container/title-container.component.ts
+++ frontend-app/src/app/app-ui/title-container/title-container.component.ts
@@ -4,15 +4,11 @@ import { Component, Input } from '@angular/core';
   selector: 'app-title-container',
   styleUrls: ['./title-container.component.scss'],
   template: `
-    <div class="title-wrapper">
-      <h1 class="title">{{ title }}</h1>
-      <ng-content *ngIf="withLeftContent" select="[titleContainerLeft]"></ng-content>
-    </div>
-    <ng-content *ngIf="withRightContent" select="[titleContainerRight]"></ng-content>
+    <h1 class="title">{{ title }}</h1>
+    <ng-content *ngIf="withContentRight"></ng-content>
   `,
 })
 export class AppTitleContainerComponent {
   @Input() title: string = '';
-  @Input() withLeftContent: string = '';
-  @Input() withRightContent: boolean = false;
+  @Input() withContentRight: boolean = false;
 }
