8545dffa8ad61ea1b648dba47612fa84e6e83d9a
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-13 21:32:20 +0300

    NOJIRA added circular button to the shared SearchContainerComponent

--- frontend-app/src/app/shared/search-container/search-container.module.ts
+++ frontend-app/src/app/shared/search-container/search-container.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SearchContainerComponent } from './search-container.component';
 import { UiModule } from '@egov/cvi-ng';
+import { AppUiModule } from 'src/app/app-ui/app-ui.module';
 
 @NgModule({
   declarations: [SearchContainerComponent],
   exports: [SearchContainerComponent],
   providers: [],
-  imports: [UiModule],
+  imports: [UiModule, AppUiModule],
 })
 export class SearchContainerModule {}
