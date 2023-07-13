4eadab7733d79429403d408ab9e258e11214fff2
Aleksandr Beliaev <aleksandr.beliaev@nortal.com>
2023-07-13 22:05:17 +0300

    NOJIRA Storybook now supports Angular static assets folder

--- frontend-app/.storybook/main.ts
+++ frontend-app/.storybook/main.ts
@@ -1,6 +1,7 @@
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../src/assets'],
   addons: [
     '@storybook/addon-links', 
     '@storybook/addon-essentials', 
