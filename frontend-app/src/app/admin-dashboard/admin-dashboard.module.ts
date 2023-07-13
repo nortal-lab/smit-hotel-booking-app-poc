235795771794d193906f4bd1aef4a95a1ec7376b
Rokas Ciapas <rokas.ciapas@nortal.com>
2023-07-13 14:58:38 +0300

    LTBENCH-844 apply title component

--- frontend-app/src/app/admin-dashboard/admin-dashboard.module.ts
+++ frontend-app/src/app/admin-dashboard/admin-dashboard.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AvailableRoomsModule } from '../shared/components/available-rooms/available-rooms.module';
 import { DateFormatModule } from '../shared/pipes/date-format.module';
+import { AppUiModule } from '../app-ui/app-ui.module';
 
 @NgModule({
   declarations: [AdminDashboardComponent],
   exports: [AdminDashboardComponent],
   providers: [],
-  imports: [UiModule, FormsModule, CommonModule, AvailableRoomsModule, DateFormatModule],
+  imports: [UiModule, FormsModule, CommonModule, AvailableRoomsModule, DateFormatModule, AppUiModule],
 })
 export class AdminDashboardModule {}
