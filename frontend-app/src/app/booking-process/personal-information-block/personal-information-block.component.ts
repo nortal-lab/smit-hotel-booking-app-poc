a55ae2e7e9ab645ad856875ec0c6510abe854f1f
Ekaterina Loginova <ekaterina.loginova@nortal.com>
2023-07-14 17:38:04 +0300

    NOJIRA|Use app-data-section component

--- frontend-app/src/app/booking-process/personal-information-block/personal-information-block.component.ts
+++ frontend-app/src/app/booking-process/personal-information-block/personal-information-block.component.ts
@@ -4,7 +4,6 @@ import { User } from '../../models/user.interface';
 @Component({
   selector: 'app-personal-information-block',
   templateUrl: './personal-information-block.component.html',
-  styleUrls: ['./personal-information-block.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PersonalInformationBlockComponent {
