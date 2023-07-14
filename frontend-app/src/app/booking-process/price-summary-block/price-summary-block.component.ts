ff85904d0af71ea4f4b91c039d3f57264d710b96
Ekaterina Loginova <ekaterina.loginova@nortal.com>
2023-07-14 17:38:45 +0300

    NOJIRA|Use app-data-section component

--- frontend-app/src/app/booking-process/price-summary-block/price-summary-block.component.ts
+++ frontend-app/src/app/booking-process/price-summary-block/price-summary-block.component.ts
@@ -4,7 +4,6 @@ import { Room } from '../../models/room.interface';
 @Component({
   selector: 'app-price-summary-block',
   templateUrl: './price-summary-block.component.html',
-  styleUrls: ['./price-summary-block.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PriceSummaryBlockComponent {
