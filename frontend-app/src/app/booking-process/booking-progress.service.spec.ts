c2cfbd289bb22da71ad5f6ddf7985c0f29320846
Rokas Ciapas <rokas.ciapas@nortal.com>
2023-07-17 10:09:03 +0300

    LTBENCH-NOTICKET remove leftover

--- frontend-app/src/app/booking-process/booking-progress.service.spec.ts
+++ frontend-app/src/app/booking-process/booking-progress.service.spec.ts
@@ -1,15 +1,10 @@
-import { TestScheduler } from 'rxjs/testing';
 import { BookingProgressService } from './booking-progress.service';
 
 describe('BookingProgressService', () => {
   let service: BookingProgressService;
-  let testScheduler: TestScheduler;
 
   beforeEach(() => {
     service = new BookingProgressService();
-    testScheduler = new TestScheduler((actual, expected) => {
-      expect(actual).toEqual(expected);
-    });
   });
 
   it('should be created', () => {
